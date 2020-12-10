0cf0a392da0d629fac6828b342102349d47adc77
Kamil Marciniak <KamilMarciniak.Kontakt@gmail.com>
2020-12-10 17:52:45 +0100

    Reversible 0 cost hooks

--- source/ReversibleHooks.cpp
+++ source/ReversibleHooks.cpp
@@ -2,6 +2,7 @@
 
 void ReversibleHooks::HookInstall(const std::string& sIdentifier, const std::string& sFuncName, unsigned int installAddress, void* addressToJumpTo, int iJmpCodeSize)
 {
+    assert(!GetHook(sIdentifier, sFuncName));
     if (m_HooksMap.find(sIdentifier) == m_HooksMap.end())
         m_HooksMap[sIdentifier] = std::vector<SReversibleHook>();
 
@@ -14,9 +15,30 @@ void ReversibleHooks::HookInstall(const std::string& sIdentifier, const std::str
     theHook.m_sIdentifier = sIdentifier;
     theHook.m_sFunctionName = sFuncName;
     theHook.m_iRealHookedAddress = installAddress;
+    theHook.m_iHookedBytes = iJmpCodeSize;
 
-    theHook.m_HookContent.jumpLocation = dwAddressToJumpTo - installAddress - x86FixedJumpSize;
-    memset(theHook.m_HookContent.possibleNops, NOP_OPCODE, iJmpCodeSize - x86FixedJumpSize);
+    theHook.m_HookContent.jumpLocation = ReversibleHooks::GetJMPLocation(installAddress, dwAddressToJumpTo);
+    memset(theHook.m_HookContent.possibleNops, NOP_OPCODE, iJmpCodeSize - ReversibleHooks::x86JMPSize);
+
+    //Copy lib function content
+    theHook.m_LibHookContent.jumpLocation = ReversibleHooks::GetJMPLocation(dwAddressToJumpTo, installAddress);
+    theHook.m_iLibHookedBytes = ReversibleHooks::x86JMPSize;
+    memcpy((void*)&theHook.m_LibOriginalFunctionContent, (void*)dwAddressToJumpTo, theHook.m_iLibHookedBytes);
+
+    auto installHook = [&](bool bVirtProtect) {
+        DWORD dwProtectHoodlum[2] = { 0 };
+        if (bVirtProtect)
+            VirtualProtect((void*)theHook.m_iRealHookedAddress, theHook.m_iHookedBytes, PAGE_EXECUTE_READWRITE, &dwProtectHoodlum[0]);
+
+        memcpy((void*)&theHook.m_OriginalFunctionContent, (void*)theHook.m_iRealHookedAddress, theHook.m_iHookedBytes);
+        memcpy((void*)theHook.m_iRealHookedAddress, &theHook.m_HookContent, theHook.m_iHookedBytes);
+
+        if (bVirtProtect)
+            VirtualProtect((void*)theHook.m_iRealHookedAddress, theHook.m_iHookedBytes, dwProtectHoodlum[0], &dwProtectHoodlum[1]);
+
+        theHook.m_bIsHooked = true;
+        theHook.m_bImguiHooked = true;
+    };
 
     const int maxBytesToProtect = std::max(iJmpCodeSize, 8);
     DWORD dwProtect[2] = { 0 };
@@ -34,33 +56,20 @@ void ReversibleHooks::HookInstall(const std::string& sIdentifier, const std::str
         }
         if (bJumpFound) {
             DWORD newInstallAddress = installAddress + i;
-            DWORD hoodlumHookAddress = *(DWORD*)(newInstallAddress + 1) + newInstallAddress + x86FixedJumpSize;
+            DWORD hoodlumHookAddress = *(DWORD*)(newInstallAddress + 1) + newInstallAddress + ReversibleHooks::x86JMPSize;
             theHook.m_iRealHookedAddress = hoodlumHookAddress;
-            theHook.m_HookContent.jumpLocation = dwAddressToJumpTo - hoodlumHookAddress - x86FixedJumpSize;
-            theHook.m_iHookedBytes = 5U;
-            DWORD dwProtectHoodlum[2] = { 0 };
-            VirtualProtect((void*)hoodlumHookAddress, x86FixedJumpSize, PAGE_EXECUTE_READWRITE, &dwProtectHoodlum[0]);
-            memcpy((void*)&theHook.m_OriginalFunctionContent, (void*)hoodlumHookAddress, x86FixedJumpSize);
-            memcpy((void*)hoodlumHookAddress, &theHook.m_HookContent, x86FixedJumpSize);
-            VirtualProtect((void*)hoodlumHookAddress, x86FixedJumpSize, dwProtectHoodlum[0], &dwProtectHoodlum[1]);
-            theHook.m_bIsHooked = true;
-            theHook.m_bImguiHooked = true;
+            theHook.m_HookContent.jumpLocation = ReversibleHooks::GetJMPLocation(hoodlumHookAddress, dwAddressToJumpTo);
+            theHook.m_iHookedBytes = ReversibleHooks::x86JMPSize;
+
+            installHook(true);
         }
         else {
             printf("HookInstall: Couldn't find the jump for address = %#.8x\n", installAddress);
-            theHook.m_iHookedBytes = iJmpCodeSize;
-            memcpy((void*)&theHook.m_OriginalFunctionContent, (void*)installAddress, iJmpCodeSize);
-            memcpy((void*)installAddress, &theHook.m_HookContent, iJmpCodeSize);
-            theHook.m_bIsHooked = true;
-            theHook.m_bImguiHooked = true;
+            installHook(false);
         }
     }
     else {
-        theHook.m_iHookedBytes = iJmpCodeSize;
-        memcpy((void*)&theHook.m_OriginalFunctionContent, (void*)installAddress, iJmpCodeSize);
-        memcpy((void*)installAddress, &theHook.m_HookContent, iJmpCodeSize);
-        theHook.m_bIsHooked = true;
-        theHook.m_bImguiHooked = true;
+        installHook(false);
     }
     VirtualProtect((void*)installAddress, maxBytesToProtect, dwProtect[0], &dwProtect[1]);
 
@@ -70,15 +79,33 @@ void ReversibleHooks::HookInstall(const std::string& sIdentifier, const std::str
 void ReversibleHooks::HookSwitch(SReversibleHook& sHook)
 {
     DWORD dwProtect[2] = { 0 };
-    auto pDst = (void*)sHook.m_iRealHookedAddress;
-    auto pSrc = (void*)&sHook.m_OriginalFunctionContent;
-    if (!sHook.m_bIsHooked) 
-        pSrc = (void*)&sHook.m_HookContent;
+    if (sHook.m_bIsHooked) {
+        auto pGTADst = (void*)sHook.m_iRealHookedAddress;
+        auto pGTASrc = (void*)&sHook.m_OriginalFunctionContent;
+        VirtualProtect(pGTADst, sHook.m_iHookedBytes, PAGE_EXECUTE_READWRITE, &dwProtect[0]);
+        memcpy(pGTADst, pGTASrc, sHook.m_iHookedBytes);
+        VirtualProtect(pGTADst, sHook.m_iHookedBytes, dwProtect[0], &dwProtect[1]);
 
+        auto pLibDst = (void*)sHook.m_iLibFunctionAddress;
+        auto pLibSrc = (void*)&sHook.m_LibHookContent;
+        VirtualProtect(pLibDst, sHook.m_iLibHookedBytes, PAGE_EXECUTE_READWRITE, &dwProtect[0]);
+        memcpy(pLibDst, pLibSrc, sHook.m_iLibHookedBytes);
+        VirtualProtect(pLibDst, sHook.m_iLibHookedBytes, dwProtect[0], &dwProtect[1]);
+    }
+    else {
+        auto pGTADst = (void*)sHook.m_iRealHookedAddress;
+        auto pGTASrc = (void*)&sHook.m_HookContent;
+        VirtualProtect(pGTADst, sHook.m_iHookedBytes, PAGE_EXECUTE_READWRITE, &dwProtect[0]);
+        memcpy(pGTADst, pGTASrc, sHook.m_iHookedBytes);
+        VirtualProtect(pGTADst, sHook.m_iHookedBytes, dwProtect[0], &dwProtect[1]);
 
-    VirtualProtect(pDst, sHook.m_iHookedBytes, PAGE_EXECUTE_READWRITE, &dwProtect[0]);
-    memcpy(pDst, pSrc, sHook.m_iHookedBytes);
-    VirtualProtect(pDst, sHook.m_iHookedBytes, dwProtect[0], &dwProtect[1]);
+        auto pLibDst = (void*)sHook.m_iLibFunctionAddress;
+        auto pLibSrc = (void*)&sHook.m_LibOriginalFunctionContent;
+        VirtualProtect(pLibDst, sHook.m_iLibHookedBytes, PAGE_EXECUTE_READWRITE, &dwProtect[0]);
+        memcpy(pLibDst, pLibSrc, sHook.m_iLibHookedBytes);
+        VirtualProtect(pLibDst, sHook.m_iLibHookedBytes, dwProtect[0], &dwProtect[1]);
+    }
+
     sHook.m_bIsHooked = !sHook.m_bIsHooked;
     sHook.m_bImguiHooked = sHook.m_bIsHooked;
 }
@@ -104,4 +131,9 @@ SReversibleHook* ReversibleHooks::GetHook(const std::string& sIdentifier, const 
 
     auto& refHook = *findResult;
     return &refHook;
+}
+
+unsigned int ReversibleHooks::GetJMPLocation(unsigned int dwFrom, unsigned int dwTo)
+{
+    return dwTo - dwFrom - ReversibleHooks::x86JMPSize;
 }
